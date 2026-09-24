c2d7648f592134fcef4ce4699eca2754ad684802
klouj-gergis <kloujgergis@gmail.com>
2026-09-24 16:59:20 +0300

    showed more info on the dashboard

--- app/dashboard/DashboardClient.js
+++ app/dashboard/DashboardClient.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { signOut } from 'next-auth/react';
 import styles from './DashboardClient.module.css';
-import { prisma } from '@/app/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 const TEAM_NAMES = {
   social: 'تيم السوشيال ميديا',
