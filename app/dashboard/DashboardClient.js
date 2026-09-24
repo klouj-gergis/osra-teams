e9290a49f44a4f51ebec6868793ebfa301826145
klouj-gergis <kloujgergis@gmail.com>
2026-09-24 07:00:08 +0300

    showed more info on the dashboard

--- app/dashboard/DashboardClient.js
+++ app/dashboard/DashboardClient.js
@@ -154,7 +154,7 @@ export default function DashboardClient({ username }) {
                           </span>
                           
                           <span>{submission.currentServed}</span>
-                          <span>{submission.skills.join(', ')}</span>
+                          <span>{submission.skills}</span>
                           <span>{submission.prevServed}</span>
 
                         </div>
